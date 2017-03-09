12938b86f55b3519dd1c1c15a631a1b97d20640c
Dónal Mac Fhionnlaoich <donal.macfhionnlaoich@ucdconnect.ie>
2017-03-09 21:30:40 +0000

    Added comments to printPlayerStatus.c and playerTypes.c

--- printPlayersStatus.c
+++ printPlayersStatus.c
@@ -5,23 +5,24 @@
  *      Author: Dónal
  */
 
-#include "structs.h"
-#include <stdio.h>
+#include "structs.h"	//Including definition of structs
+#include <stdio.h>		//Including stdio library for printf function.
 
+//Function to print out all details of every player.
 void printPlayersStatus(struct player_type player [6], int n)
 {
-	for(int i=0; i<n; ++i)
+	for(int i=0; i<n; ++i)	//For loop to index through each player up to the final player.
 	{
-		printf("\n-PLAYER %d-\n", i+1);
-		printf("Name:\t\t%s\n", player[i].name);
-		printf("Type:\t\t%s\n", player[i].type);
-		printf("Life Points:\t%.1f\n", player[i].lifepoints);
-		printf("Smartness:\t%d\n", player[i].smartness);
-		printf("Strength:\t%d\n", player[i].strength);
-		printf("Magic Skills:\t%d\n", player[i].magicskills);
-		printf("Luck:\t\t%d\n", player[i].luck);
-		printf("Dexterity:\t%d\n", player[i].dexterity);
-		printf("Location:\tSlot %d\n", player[i].slot);
-		printf("Slot Name:\t%s\n", player[i].currentSlot);
+		printf("\n-PLAYER %d-\n", i+1);							//Indicating which player the following details are related to.
+		printf("Name:\t\t%s\n", player[i].name);				//Printing player name.
+		printf("Type:\t\t%s\n", player[i].type);				//Printing what type of player the selected player is.
+		printf("Life Points:\t%.1f\n", player[i].lifepoints);	//Printing the value of the lifepoints of the player.
+		printf("Smartness:\t%d\n", player[i].smartness);		//Printing the value of the smartness value of the player selected.
+		printf("Strength:\t%d\n", player[i].strength);			//Printing the value of the strength of the selected player.
+		printf("Magic Skills:\t%d\n", player[i].magicskills);	//Printing the value of the magic skills of the slected player.
+		printf("Luck:\t\t%d\n", player[i].luck);				//Printing the value of the luck of the slected player.
+		printf("Dexterity:\t%d\n", player[i].dexterity);		//Printing the value of the dexterity of the slected player.
+		printf("Location:\tSlot %d\n", player[i].slot);			//Printing the slot number of the player.
+		printf("Slot Name:\t%s\n", player[i].currentSlot);		//Printing the slot type of the player.
 	}
 }
