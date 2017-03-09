9eb8df69d23961250f72e49b35bc8b1be6e4a293
Dónal Mac Fhionnlaoich <donal.macfhionnlaoich@ucdconnect.ie>
2017-03-09 20:37:21 +0000

    Added comments to playerAction.c and attack.c

--- attack.c
+++ attack.c
@@ -5,18 +5,19 @@
  *      Author: Dónal
  */
 
-#include "structs.h"
+#include "structs.h"	//Including the definition of the structs.
 
+//Function for when one player decides to attack another.
 void attack(struct player_type *attacker, struct player_type *attacked)
 {
-	float x = attacked->strength;
+	float x = attacked->strength;	//float to hold the value of the attacked players strength.
 
-	if(attacked->strength <= 70)
-	{
+	if(attacked->strength <= 70)		//If the attacked players strength is less than or equal to 70 then the attacked player gets half
+	{									//of their strength subtracted from their life points.
 		attacked->lifepoints -= (0.5 * x);
 	}
-	else if(attacked->strength > 70)
-	{
+	else if(attacked->strength > 70)	//If the attacked players strength is greater than 70 then the player who initiated the attack gets
+	{									//three tenths of the attacked players strength taken from their lifepoints.
 		attacker->lifepoints -= (0.3 * x);
 	}
 }
--- playerAction.c
+++ playerAction.c
@@ -5,38 +5,39 @@
  *      Author: D�nal
  */
 
-#include "structs.h"
-#include "Functions.h"
+#include "structs.h"	//Including definition of structs.
+#include "Functions.h"	//Including prototypes of functions.
 
+//Function to carry out player's actions based on their selection.
 void playerAction(int y, int b,struct player_type *player,struct slot_type *slot,int i,int n)
 {
-	int j,x;
+	int j,x;	//Declaring integers. j used as indexing variable. x is used to hold the value of a member of a player.
 
-	if(b == 0){
-		moveslot(&player[i], &slot[y-2], i);
-		slot[y-1].taken = 0;
-		player[i].slot -= 1;
+	if(b == 0){			//If player has chosen to move backwards one slot.
+		moveslot(&player[i], &slot[y-2], i);	//Calling moveslot function to move player.
+		slot[y-1].taken = 0;					//Changing taken value of the slot that was previously occupied to signify that it is now empty.
+		player[i].slot -= 1;					//Decrementing the slot member of the player since they have moved back one slot.
 	}
 
-	else if(b == 1){
-		moveslot(&player[i], &slot[y], i);
-		slot[y-1].taken = 0;
-		player[i].slot += 1;
+	else if(b == 1){	//If player has chosen to move forwards one slot.
+		moveslot(&player[i], &slot[y], i);		//Calling moveslot function to move player.
+		slot[y-1].taken = 0;					//Changing taken value of the slot that was previously occupied to signify that it is now empty.
+		player[i].slot += 1;					//Incrementing the slot member of the player since they have moved forward one slot.
 	}
-	else if(b == 3){
-		for(j=0;j<n;j++){
-			x = player[j].slot;
-			if(x == (y + 1)){
-				attack(&player[i], &player[j]);
-			}
+	else if(b == 3){	//If player has chosen to attack the player that is in the slot directly infront of them.
+		for(j=0;j<n;j++){						//For loop to find which player the player is attacking.
+			x = player[j].slot;					//Assigning value of player member slot to x.
+			if(x == (y + 1)){					//Checking whether the jth player is in the slot that is directly after the slot the attacking player is in.
+				attack(&player[i], &player[j]);	//When the player that is directly after the attacking player is found, the attack function is called passing in both
+			}										//the attacking (i-th) and attacked (j-th) player.						
 		}
 	}
-	else if(b == 4){
-		for(j=0;j<n;j++){
-			x = player[j].slot;
-			if(x == (y - 1)){
-				attack(&player[i], &player[j]);
-			}
+	else if(b == 4){	//If player has chosen to attack the player that is in the slot directly behind of them.
+		for(j=0;j<n;j++){						//For loop to find which player the player is attacking.
+			x = player[j].slot;					//Assigning value of player member slot to x.
+			if(x == (y - 1)){					//Checking whether the jth player is in the slot that is directly before the slot the attacking player is in.
+				attack(&player[i], &player[j]);	//When the player that is directly before the attacking player is found, the attack function is called passing in both
+			}										//the attacking (i-th) and attacked (j-th) player.
 		}
 	}
 }
