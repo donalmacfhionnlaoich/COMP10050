51862fc14a1522e3c15ab335ecb5f2fd7ce53aeb
Daniel139 <Daniel139@users.noreply.github.com>
2017-03-10 16:20:03 +0000

    Redundant comments removed

--- main.c
+++ main.c
@@ -60,168 +60,166 @@ int main(void){
 	/* ---User input of player name and type--- */
 	puts("\nA players name can have a maximum length of 19 characters.");
 	puts("There are 4 types of player: Elf, Human, Ogre and Wizard.");	//Giving information to user on the possible types of player_type and name specs.
-	if((n <= PLAYER_MAX && s <= SLOT_MAX) && (n <= s)){
-		for(i=0; i<n; i++){
-			printf("\nEnter Player %d's name: ", i+1);
-			scanf("%19s", player[i].name);	//Ensuring the maximum of characters of name is at most 19 so that there is one space for the null terminator.
-			printf("\n----------Enter Player %d's type---------\n", i+1);
-			printf("1 - Elf, 2 - Human, 3 - Ogre, 4 - Wizard:\n");
-			scanf("%d", &typeNum);		// User input for player type
-			while(typeNum < 1 || typeNum > 4){	// Loops while entered digit is not 1-4
-				puts("Sorry but you must enter a number between 1 & 4:\n");
-				scanf("%d", &typeNum);
-			}
-			player[i].lifepoints = 100.0;		// Sets everybody's LP's too 100.0 as stated
-												// Must be floating point for attack calculations
-			/* Switch that copies player's type and respective capabilities to each struct member */
-			switch(typeNum){
-				case 1:
-					strcpy(player[i].type, "Elf");
-					elf(&player[i]);		// Elf function (see playerTypes.c)
-					break;
+	for(i=0; i<n; i++){
+		printf("\nEnter Player %d's name: ", i+1);
+		scanf("%19s", player[i].name);	//Ensuring the maximum of characters of name is at most 19 so that there is one space for the null terminator.
+		printf("\n----------Enter Player %d's type---------\n", i+1);
+		printf("1 - Elf, 2 - Human, 3 - Ogre, 4 - Wizard:\n");
+		scanf("%d", &typeNum);		// User input for player type
+		while(typeNum < 1 || typeNum > 4){	// Loops while entered digit is not 1-4
+			puts("Sorry but you must enter a number between 1 & 4:\n");
+			scanf("%d", &typeNum);
+		}
+		player[i].lifepoints = 100.0;		// Sets everybody's LP's too 100.0 as stated
+											// Must be floating point for attack calculations
+		/* Switch that copies player's type and respective capabilities to each struct member */
+		switch(typeNum){
+			case 1:
+				strcpy(player[i].type, "Elf");
+				elf(&player[i]);		// Elf function (see playerTypes.c)
+				break;
 
-				case 2:
-					strcpy(player[i].type, "Human");
-					human(&player[i]);		// Human function (see playerTypes.c)
-					break;
-				case 3:
-					strcpy(player[i].type, "Ogre");
-					ogre(&player[i]);		// Ogre function (see playerTypes.c)
-					break;
-				case 4:
-					strcpy(player[i].type, "Wizard");
-					wizard(&player[i]);		// Wizard function (see playerTypes.c)
-					break;
-			}
+			case 2:
+				strcpy(player[i].type, "Human");
+				human(&player[i]);		// Human function (see playerTypes.c)
+				break;
+			case 3:
+				strcpy(player[i].type, "Ogre");
+				ogre(&player[i]);		// Ogre function (see playerTypes.c)
+				break;
+			case 4:
+				strcpy(player[i].type, "Wizard");
+				wizard(&player[i]);		// Wizard function (see playerTypes.c)
+				break;
 		}
-		slotnum(s, a);		// Function that shuffles array a to place players on random slots (see slotFunctions.c)
-		/* Randomly assigns slot type to each slot */
-		for(i=0; i<s; i++){
-			x = rand() % 3 + 1;
-			slot[i].numb = x;
-			slotname(&slot[i], x);	// Function that names slots (see slotFunctions.c)
-			slot[i].numb = a[i];	// Assigns random value stored in a[] for allocation of player to slot
-			slot[i].taken = 0;
-		}
-		for(i=0; i<n; i++){
-			player[i].slot = slot[i].numb;
-			y = player[i].slot;
-			strcpy(slot[y-1].occupant, player[i].name);
-			moveslot(player, &(slot[y-1]), i);	// Function that assigns player to slot (see slotFunctions.c)
-		}
-		printPlayersStatus(player, n);			// Function to print each players details and location (see printPLayerStatus.c)
-		printf("\n");
-		/* Prints slots */
-		for(i=0;i<s;i++){
-			printf("Slot: %-2d - type: %-12s - Taken: %d - Occupant: %s\n", i+1, slot[i].type, slot[i].taken, slot[i].occupant);
-		}
-		/* Code to ask user what move they would like to make i.e. move slot or attack */
-		for(i=0; i<n; i++){
-			y = player[i].slot;		// Assign slot number to y for comparison with other slots
-			printf("\nPlayer %d - %s\nYou are on slot %d, a %s slot\n", i+1, player[i].name, player[i].slot, player[i].currentSlot);
-			/* Option for player with no opponents on adjacent slots */
-			if((y != 1) && (y != s)) {
-				if(slot[y-2].taken == 0 && slot[y].taken == 0 ){
-					printf("Would you like to move to the next or the previous slot\nor attack the nearest player?\n");
-					printf("Press 1 for forward move or 0 for backward move or 7 for nearest player attack\n");
+	}
+	slotnum(s, a);		// Function that shuffles array a to place players on random slots (see slotFunctions.c)
+	/* Randomly assigns slot type to each slot */
+	for(i=0; i<s; i++){
+		x = rand() % 3 + 1;
+		slot[i].numb = x;
+		slotname(&slot[i], x);	// Function that names slots (see slotFunctions.c)
+		slot[i].numb = a[i];	// Assigns random value stored in a[] for allocation of player to slot
+		slot[i].taken = 0;
+	}
+	for(i=0; i<n; i++){
+		player[i].slot = slot[i].numb;
+		y = player[i].slot;
+		strcpy(slot[y-1].occupant, player[i].name);
+		moveslot(player, &(slot[y-1]), i);	// Function that assigns player to slot (see slotFunctions.c)
+	}
+	printPlayersStatus(player, n);			// Function to print each players details and location (see printPLayerStatus.c)
+	printf("\n");
+	/* Prints slots */
+	for(i=0;i<s;i++){
+		printf("Slot: %-2d - type: %-12s - Taken: %d - Occupant: %s\n", i+1, slot[i].type, slot[i].taken, slot[i].occupant);
+	}
+	/* Code to ask user what move they would like to make i.e. move slot or attack */
+	for(i=0; i<n; i++){
+		y = player[i].slot;		// Assign slot number to y for comparison with other slots
+		printf("\nPlayer %d - %s\nYou are on slot %d, a %s slot\n", i+1, player[i].name, player[i].slot, player[i].currentSlot);
+		/* Option for player with no opponents on adjacent slots */
+		if((y != 1) && (y != s)) {
+			if(slot[y-2].taken == 0 && slot[y].taken == 0 ){
+				printf("Would you like to move to the next or the previous slot\nor attack the nearest player?\n");
+				printf("Press 1 for forward move or 0 for backward move or 7 for nearest player attack\n");
+				scanf("%d",&b);
+				while(b!=0 && b!=1 && b!=7)
+				{
+					puts("That was not a valid option. Please enter a valid option.");
 					scanf("%d",&b);
-					while(b!=0 && b!=1 && b!=7)
-					{
-						puts("That was not a valid option. Please enter a valid option.");
-						scanf("%d",&b);
-					}
-					playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
 				}
-				/* Option for player that has opponent ahead and empty slot behind */
-				else if(slot[y-2].taken == 0 && slot[y].taken == 1 ){
-					printf("Would you like to move forwards and attack or move backward to the previous slot?\n");
-					printf("Press 3 for forward attack or 0 for backward move\n");
-					scanf("%d", &b);
-					while(b!=0 && b!=3)
-					{
-						puts("That was not a valid option. Please enter a valid option.");
-						scanf("%d",&b);
-					}
-					playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
-				}
-				/* Option for player that has opponent behind and empty slot ahead */
-				else if(slot[y-2].taken == 1 && slot[y].taken == 0 ){
-					printf("Would you like to move forwards to the next slot or move backward and attack?\n");
-					printf("Press 1 for forward move or 4 for backward attack\n");
-					scanf("%d", &b);
-					while(b!=1 && b!=4)
-					{
-						puts("That was not a valid option. Please enter a valid option.");
-						scanf("%d",&b);
-					}
-					playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
-				}
-				/* Option for player that has opponents ahead and behind */
-				else if(slot[y-2].taken == 1 && slot[y].taken == 1 ){
-					printf("Would you like to move forwards and attack or move backward and attack?\n");
-					printf("Press 3 for forward attack or 4 for backward attack\n");
-					scanf("%d", &b);
-					while(b!=3 && b!=4)
-					{
-						puts("That was not a valid option. Please enter a valid option.");
-						scanf("%d",&b);
-					}
-					playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
-				}
+				playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
 			}
-			/* If player is on the first slot */
-			else if(y == 1) {
-				/* And next slot is free*/
-				if(slot[y].taken == 0 ){
-					printf("Move to the next slot or attack nearest player?\n");
-					printf("Press 1 for forward move or 5 for nearest player attack\n");
-					scanf("%d", &b);
-					while(b!=1 && b!=5)
-					{
-						puts("That was not a valid option. Please enter a valid option.");
-						scanf("%d",&b);
-					}
-					playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
+			/* Option for player that has opponent ahead and empty slot behind */
+			else if(slot[y-2].taken == 0 && slot[y].taken == 1 ){
+				printf("Would you like to move forwards and attack or move backward to the previous slot?\n");
+				printf("Press 3 for forward attack or 0 for backward move\n");
+				scanf("%d", &b);
+				while(b!=0 && b!=3)
+				{
+					puts("That was not a valid option. Please enter a valid option.");
+					scanf("%d",&b);
 				}
-				/* And next slot is free */
-				else if(slot[y].taken == 1 ){
-					printf("Attack player on slot 2?\n");
-					printf("Press 3 to confirm\n");
-					scanf("%d", &b);
-					while(b!=3)
-					{
-						puts("That was not a valid option. Please enter a valid option.");
-						scanf("%d",&b);
-					}
-					playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
-				}
+				playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
 			}
-			/* If player is on the last slot */
-			else if(y == s) {
-				/* And previous slot is empty */
-				if(slot[y-2].taken == 0 ){
-					printf("This is the last slot, move to the previous slot or attack nearest player?\n");
-					printf("Press 0 for backward move or 6 for nearest player attack\n");
-					scanf("%d", &b);
-					while(b!=0 && b!=6)
-					{
-						puts("That was not a valid option. Please enter a valid option.");
-						scanf("%d",&b);
-					}
-					playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
+			/* Option for player that has opponent behind and empty slot ahead */
+			else if(slot[y-2].taken == 1 && slot[y].taken == 0 ){
+				printf("Would you like to move forwards to the next slot or move backward and attack?\n");
+				printf("Press 1 for forward move or 4 for backward attack\n");
+				scanf("%d", &b);
+				while(b!=1 && b!=4)
+				{
+					puts("That was not a valid option. Please enter a valid option.");
+					scanf("%d",&b);
 				}
-				/* And previous slot is taken */
-				else if(slot[y-2].taken == 1 ){
-					printf("This is the last slot, attack player on slot %d?\n", s-1);
-					printf("Press 4 to confirm\n");
-					scanf("%d", &b);
-					while(b!=4)
-					{
-						puts("That was not a valid option. Please enter a valid option.");
-						scanf("%d",&b);
-					}
-					playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
+				playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
+			}
+			/* Option for player that has opponents ahead and behind */
+			else if(slot[y-2].taken == 1 && slot[y].taken == 1 ){
+				printf("Would you like to move forwards and attack or move backward and attack?\n");
+				printf("Press 3 for forward attack or 4 for backward attack\n");
+				scanf("%d", &b);
+				while(b!=3 && b!=4)
+				{
+					puts("That was not a valid option. Please enter a valid option.");
+					scanf("%d",&b);
 				}
+				playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
+			}
+		}
+		/* If player is on the first slot */
+		else if(y == 1) {
+			/* And next slot is free*/
+			if(slot[y].taken == 0 ){
+				printf("Move to the next slot or attack nearest player?\n");
+				printf("Press 1 for forward move or 5 for nearest player attack\n");
+				scanf("%d", &b);
+				while(b!=1 && b!=5)
+				{
+					puts("That was not a valid option. Please enter a valid option.");
+					scanf("%d",&b);
+				}
+				playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
+			}
+			/* And next slot is free */
+			else if(slot[y].taken == 1 ){
+				printf("Attack player on slot 2?\n");
+				printf("Press 3 to confirm\n");
+				scanf("%d", &b);
+				while(b!=3)
+				{
+					puts("That was not a valid option. Please enter a valid option.");
+					scanf("%d",&b);
+				}
+				playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
+			}
+		}
+		/* If player is on the last slot */
+		else if(y == s) {
+			/* And previous slot is empty */
+			if(slot[y-2].taken == 0 ){
+				printf("This is the last slot, move to the previous slot or attack nearest player?\n");
+				printf("Press 0 for backward move or 6 for nearest player attack\n");
+				scanf("%d", &b);
+				while(b!=0 && b!=6)
+				{
+					puts("That was not a valid option. Please enter a valid option.");
+					scanf("%d",&b);
+				}
+				playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
+			}
+			/* And previous slot is taken */
+			else if(slot[y-2].taken == 1 ){
+				printf("This is the last slot, attack player on slot %d?\n", s-1);
+				printf("Press 4 to confirm\n");
+				scanf("%d", &b);
+				while(b!=4)
+				{
+					puts("That was not a valid option. Please enter a valid option.");
+					scanf("%d",&b);
+				}
+				playerAction(y ,b ,player ,slot ,i ,n ,s);	// Function for move or attack (see playerAction.c)
 			}
 		}
 		// Prints players name, type and lifepoints after all turns have been taken
@@ -231,26 +229,11 @@ int main(void){
 		{
 			printf("%-19s (%s, %0.1f)\n",player[i].name,player[i].type,player[i].lifepoints);
 		}
-
-	}
-	/* Error messages for incorrect input of player or slot number */
-	else if(n > PLAYER_MAX){
-		printf("Sorry maximum number of players is 6!\n");
-		exit(1);
-	}
-	else if(s > SLOT_MAX){
-		printf("Sorry maximum number of slots is 20!\n");
-		exit(1);
-	}
-	else{
-		printf("Sorry but there needs to at least one slot per player\n");
-		exit(1);
 	}
 	/* free memory previously allocated with calloc */
 	free(a);
 	free(player);
 	free(slot);
-	return (0);
-	
+	return (0);	
 }
 
