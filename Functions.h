3c09c574723e98b0fb033bbd7d16257f0babfd51
Daniel139 <Daniel139@users.noreply.github.com>
2017-03-10 02:04:51 +0000

    UPDA

--- Functions.h
+++ Functions.h
@@ -21,7 +21,7 @@ void slotname(struct slot_type *slot, int x);
 void city(struct player_type *player);
 void hill(struct player_type *player);
 void attack(struct player_type *attacker, struct player_type *attacked);
-void playerAction(int y, int b,struct player_type player[6],struct slot_type slot[20],int i,int n);
+void playerAction(int y, int b,struct player_type player[6],struct slot_type slot[20],int i,int n,int s);
 
 
 void human(struct player_type *player);
--- playerAction.c
+++ playerAction.c
@@ -9,9 +9,9 @@
 #include "Functions.h"	//Including prototypes of functions.
 
 //Function to carry out player's actions based on their selection.
-void playerAction(int y, int b,struct player_type *player,struct slot_type *slot,int i,int n)
+void playerAction(int y, int b,struct player_type *player,struct slot_type *slot,int i,int n,int s)
 {
-	int j,x;	//Declaring integers. j used as indexing variable. x is used to hold the value of a member of a player.
+	int j,x, k, l;	//Declaring integers. j used as indexing variable. x is used to hold the value of a member of a player.
 
 	if(b == 0){			//If player has chosen to move backwards one slot.
 		moveslot(&player[i], &slot[y-2], i);	//Calling moveslot function to move player.
@@ -38,6 +38,52 @@ void playerAction(int y, int b,struct player_type *player,struct slot_type *slot
 			if(x == (y - 1)){					//Checking whether the jth player is in the slot that is directly before the slot the attacking player is in.
 				attack(&player[i], &player[j]);	//When the player that is directly before the attacking player is found, the attack function is called passing in both
 			}										//the attacking (i-th) and attacked (j-th) player.
+		}
+	}
+	else if(b == 5){	//If player is in first slot and has chosen to attack the player that is in the nearest slot ahead of them.
+		for(j=1;j<s;j++){						//For loop checking slots 2 upwards until it finds one thats inhabited
+			if(slot[j].taken == 1){				//When it finds one 
+				for(k=0; k<n; k++){				// it loops through the players 
+					if(player[k].slot == j+1)	// to find the inhabitant.
+						attack(&player[i], &player[k]);	//When the player that is nearest the attacking player is found, the attack function is called passing in both
+					}									//the attacking (i-th) and attacked (j-th) player.
+			}
+		}
+	}
+	else if(b == 6){	//If player is in last slot and has chosen to attack the player that is in the nearest slot behind of them.
+		for(j=(s-1);j>-1;j--){						//For loop checking from last slot backwards until it finds one thats inhabited
+			if(slot[j].taken == 1){				//When it finds one 
+				for(k=0; k<n; k++){				// it loops through the players 
+					if(player[k].slot == j+1)	// to find the inhabitant.
+						attack(&player[i], &player[k]);	//When the player that is nearest the attacking player is found, the attack function is called passing in both
+					}									//the attacking (i-th) and attacked (j-th) player.
+			}
+		}
+	}
+	else if(b == 7){	//If player is in any slot with two vacant adjacent slots and has chosen to attack the player that is in the nearest slot to them.
+		x = player[i].slot;						//Assigning value of player member slot to x.
+		for(j=i+1;j<s;j++){						//Loops through slots in front of player
+			if(slot[j].taken == 0){				
+				k = 0;							//Incase no player is found k will stay at 0 allowing calculations on line 81 be valid
+			}
+			else if(slot[j].taken == 1){		// If player is found k will be assigned the slot number
+				k = j+1;
+			}
+		}
+		for(j=i-1;j>-1;j--){					//Loops through slots in behind player
+			if(slot[j].taken == 0){
+				l = 0;							//Incase no player is found l will stay at 0 allowing calculations on line 81 to be valid
+						}
+			else if(slot[j].taken == 1){
+				l = j+1;						// If player is found l will be assigned the slot number
+			}
+		}
+		if((x-k) > (x-l)){						// This calulation will determine if there are players each side, which player is closest
+			attack(&player[i], &player[l]);		// if true k is greater therefore l is closer
+		}
+		else{
+			attack(&player[i], &player[k]);		//else for when l is greater therefore k is closer
+			return;
 		}
 	}
 }
